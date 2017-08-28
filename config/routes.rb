6661c7934c304c06210b435865433952c356229b
Alessandro Cappellato <acappellato1@gmail.com>
2017-08-28 14:02:19 +0100

    created users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/destroy'
+
+  get 'users/create'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
