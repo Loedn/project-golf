1f8694b56c0c8f3c1fa432729b3753bdd89bf05a
Alessandro Cappellato <acappellato1@gmail.com>
2017-08-28 14:35:26 +0100

    added some validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,12 @@
 class Course < ApplicationRecord
   belongs_to :owner
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :image, presence: true
+  # validates :description, presence: true
+  # validates :email, presence: true
+  # validates :phone, presence: true
+  # validates :timeslots, presence: true
+
 end
