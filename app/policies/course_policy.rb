d9b86863b2f8def4994bb294b3a42d871e42d19f
Alessandro Cappellato <acappellato1@gmail.com>
2017-08-29 14:48:29 +0100

    maybe

---
+++ app/policies/course_policy.rb
@@ -0,0 +1,26 @@
+class CoursePolicy < ApplicationPolicy
+
+  def create?
+    return true
+  end
+
+  def edit?
+    record.owner == user
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy?
+    record.owner == user
+  end
+
+
+  class Scope < Scope
+    def resolve
+      scope
+    end
+
+  end
+end
