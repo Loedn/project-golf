cdb49510469cefad8864449aae6e9b59bd6a36cc
Alessandro Cappellato <acappellato1@gmail.com>
2017-08-29 16:24:44 +0100

    solved fb stuff

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,21 +10,21 @@ class Course < ApplicationRecord
   # validates :timeslots, presence: true
   mount_uploader :photo, PhotoUploader
 
-  after_create :set_timeslots
+  # after_create :set_timeslots
 
 private
 
-  def set_timeslots
-    array = []
-    hour = 9
-    counter = 0
-    while hour < 17
-        array << "#{hour == 9 ? '0' : ''}#{hour}:#{counter < 10 ? '0' : ''}#{counter}"
-        counter += 5
-        counter == 60 ? counter = 0 : ''
-        counter == 0 ? hour += 1 : ''
-    end
-
-    self.update(timeslots: array)
-  end
+  # def set_timeslots
+  #   array = []
+  #   hour = 9
+  #   counter = 0
+  #   while hour < 17
+  #       array << "#{hour == 9 ? '0' : ''}#{hour}:#{counter < 10 ? '0' : ''}#{counter}"
+  #       counter += 5
+  #       counter == 60 ? counter = 0 : ''
+  #       counter == 0 ? hour += 1 : ''
+  #   end
+  #
+  #   self.update(timeslots: array)
+  # end
 end
