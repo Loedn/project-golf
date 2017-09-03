b6c1a9ded1af9322a122b58277e9d932c64b7eaf
Jessica Choong <jchoongnw@gmail.com>
2017-09-03 23:16:19 +0100

    added a fav course to users for user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903214722) do
+ActiveRecord::Schema.define(version: 20170903220929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20170903214722) do
     t.string   "photo"
     t.string   "location"
     t.string   "description"
+    t.string   "favourite_course"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
