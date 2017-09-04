a783471c162de47253fcb55a42f73dda321fb72f
jordan araujo <jordasg@gmail.com>
2017-09-04 18:21:23 +0100

    discard

--- app/controllers/hole_scores_controller.rb
+++ app/controllers/hole_scores_controller.rb
@@ -4,11 +4,7 @@ class HoleScoresController < ApplicationController
   def create
     # binding.pry
     @holescore = HoleScore.new(user_id: params[:user_id], event_id: params[:event_id], hole_id: params[:hole_id], strikes: params["hole_score"]["strikes"])
-    if @holescore.save
-      redirect_to root_path
-    else
-      redirect_to user_path
-    end
+    @holescore.save
   end
 
   private
