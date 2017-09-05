727100600f9df113277cf3dbec5e4b4f278e3fee
Alessandro Cappellato <acappellato1@gmail.com>
2017-09-05 18:55:06 +0100

    done

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,9 +3,13 @@ class FriendshipsController < ApplicationController
 
   end
   def create
-    @friendship = Friendship.create(user_id: current_user.id, friend_id: FriendRequest.find(params[:friend_request_id]).user.id)
-    redirect_to :back
+    @friendship = Friendship.new(user_id: current_user.id, friend_id: FriendRequest.find(params[:friend_request_id]).user.id)
+    if @friendship.save
+      @notification = Notification.where(friend_request_id: params[:friend_request_id])
+      @notification.update(read: true)
+    end
     authorize @friendship
+    redirect_to :back
   end
   def destroy
     @friendship = Friendship.find(params[:id])
