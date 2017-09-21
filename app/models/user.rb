cd83ee3164cc35e11b0f53ffd0b0535e5d0168e4
Alessandro Cappellato <acappellato1@gmail.com>
2017-09-21 19:00:06 +0200

    calendare integration w/ https://github.com/icalendar/icalendar/tree/master ?

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,7 @@ include PgSearch
     "#{first_name} #{last_name}"
   end
 
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
